f7ba7359df4e19b01cc738c09be0fba2b7807816
User3426 <tristanrage.adresse@gmail.com>
2026-05-26 17:53:38 +0200

    Phase 17 : ORM (utilisation du lien entre les tables, partie front).

--- src/Form/VisiteType.php
+++ src/Form/VisiteType.php
@@ -3,11 +3,13 @@
 namespace App\Form;
 
 use App\Entity\Visite;
+use App\Entity\Environnement;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use DateTime;
 
 class VisiteType extends AbstractType
@@ -30,6 +32,12 @@ class VisiteType extends AbstractType
             ])
             ->add('tempmax', null, [
                 'label' => 't° max'
+            ])
+            ->add('environnements', EntityType::class, [
+                'class' => Environnement::class,
+                'choice_label' => 'nom',
+                'multiple' => true,
+                'required' => false
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Enregistrer'
